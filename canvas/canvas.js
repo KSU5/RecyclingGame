c668112c8c01ecfe91b3b7c04b29052d6665730e
Lyl785 <44791475+Lyl785@users.noreply.github.com>
2020-10-21 19:55:57 -0400

    Add files via upload

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -18,28 +18,104 @@ function vector_scalar_multiplicaton(v1, s){
 function vector_length(v1){
     return Math.sqrt(v1.x * v1.x + v1.y * v1.y);
 }
-
-function vector_normalize(v1){
-    
-}*/
+*/
 
 
+
+
+
+
+
+document.addEventListener("keydown",key_down_handler,false);
+document.addEventListener("keyup",key_up_handler,false);
+document.addEventListener("keypress",key_press_handler,false);
 
 
 var canvas = document.getElementById("game_layer");
 var context = canvas.getContext("2d");
+
+var move_left = false;
+var move_right = false;
+var weapon_up = false;
+var weapon_down = false;
+var weapon_shoot = false;
+
+function key_down_handler(event){
+    if(event.keyCode == 39){
+        move_right = true;
+    }
+    else if(event.keyCode == 37){
+        move_left = true;
+    }
+
+}
+
+function key_up_handler(event){
+    if(event.keyCode == 39){
+        move_right = false;
+    }
+    else if(event.keyCode == 37){
+        move_left = false;
+    }
+
+}
+
+function key_press_handler(event){
+    if(event.keyCode == 38){
+        weapon_up = true;
+    }   
+    else if(event.keyCode == 40){
+        weapon_down = true;
+    }
+    else if(event.keyCode == 32){
+        weapon_shoot = true;
+    }
+
+}
+
+function Projectile(x, y){
+    this.x = x;
+    this.y = y;
+    this.width = 1;
+    this.height = 5;
+    this.speedy = 3;
+}
+Projectile.prototype.draw = function(){
+    context.fillStyle = "blue";
+    context.fillRect(this.x, this.y, this.width, this.height);
+}
+Projectile.prototype.update = function(){
+    if(weapon_shoot){
+        this.x =  player_x;
+        this.y -= this.speedy;
+    }
+}
+
 
 function Player(x, y ){
     this.x = x;
     this.y = y;
     this.width = 20;
     this.height = 15;
-    this.direction = -1;
+    this.speedx = 0.6;
+    this.hp = 3;
 }
 Player.prototype.draw = function(){
     context.fillStyle = "gold";
     context.fillRect(this.x, this.y, this.width, this.height);
 };
+
+Player.prototype.update = function(){
+    if (move_right){
+        this.x += this.speedx;
+        player_x = this.x;
+    }
+    else if(move_left){
+        this.x -= this.speedx;
+        player_x = this.x;
+    }
+    
+}
 
 function Enemy(x, y){
     this.x = x;
@@ -47,20 +123,25 @@ function Enemy(x, y){
     this.width = 10;
     this.height = 10;
     this.direction = -1;
+    this.speedy = 0.1
 }
 Enemy.prototype.draw = function(){
     context.fillStyle = "green";
     context.fillRect(this.x, this.y, this.width, this.height);
 };
+
 Enemy.prototype.update = function(){
-    this.y = this. y + this.direction;
+    this.y = this. y + this.direction * this.speedy;
     if (this. y <= 0 || this.y + this.height >= canvas.height){
         this.direction = this.direction * -1;
     }
 
 }
 
-var player1 = new Player(100, 175);
+var player_x = canvas.width/2;
+var player_y = 175;
+var player1 = new Player(player_x, player_y);
+var projectile = new Projectile(player_x,player_y);
 var enemy1 = new Enemy(20,25);
 var enemy2 = new Enemy(50,25);
 var enemy3 = new Enemy(100,25);
@@ -72,7 +153,15 @@ function execution() {
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
+    player1.update();
     player1.draw();
+
+  
+    if(weapon_shoot){
+        projectile.update();
+        projectile.draw();
+    }
+       
 
     enemy1.update();
     enemy1.draw();
